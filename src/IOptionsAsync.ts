485e769a4f2edf57719b2e99e074cf62000602a1
kabeleced77 <kabeleced@arcor.de>
2021-09-21 12:12:01 +0200

    feat: IOptionsAsync: changed name of method add() to option() reflecting the actual behaviour in providing an option; added comments for each method

--- src/IOptionsAsync.ts
+++ src/IOptionsAsync.ts
@@ -1,5 +1,17 @@
 import { IOptionAsync } from './IOptionAsync'
-export interface IOptionsAsync {
-  options(): Promise<IOptionAsync[]>
-  add(name: string, value: string, defaultValue: string): void
+
+/**
+ * This asynchronous interface describes the possibility for an object oriented options management: creation and accessing.
+ */
+export interface IOptionsAsync<TName = string, TValue = string> {
+  /**
+   * Get all available options.
+   */
+  options(): Promise<IOptionAsync<TName, TValue>[]>
+  /**
+   * Get or create given option by name. Optionally a default value can be given. If there is no option saved a new one will be created using the given or a standard-default value. The actual value can be changed later of course.
+   * @param optionName name of option its values (default and actual value) are saved by.
+   * @param [defaultValue] default value of option. If not given a standard-default value will be used.
+   */
+  option(optionName: TName, defaultValue?: TValue): Promise<IOptionAsync<TName, TValue>>
 }
--- test/IOptionsAsync.test.ts
+++ test/IOptionsAsync.test.ts
@@ -1,11 +1,11 @@
-import { IOptionAsync } from "../src/IOptionAsync";
-import { IOptionsAsync } from "../src/IOptionsAsync";
+import { IOptionAsync } from '../src/IOptionAsync'
+import { IOptionsAsync } from '../src/IOptionsAsync'
 
-describe("IOptionsAsync interface", () => {
-  it("ensures interface description", async () => {
-    ({
+describe('IOptionsAsync interface', () => {
+  it('ensures interface description', async () => {
+    ;({
       options: () => new Promise<IOptionAsync[]>(() => []),
-      add: (name: string, value: string, defaultValue: string) => {},
-    } as IOptionsAsync);
-  });
-});
+      option: (name: string, value: string, defaultValue: string) => {},
+    } as IOptionsAsync)
+  })
+})
